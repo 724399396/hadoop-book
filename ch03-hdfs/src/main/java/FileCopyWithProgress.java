9f84b1b2183885d14b51d60c2638ac2197e6a03e
Wei Li <liwei72439936@gmail.com>
2018-07-18 18:12:20 +0800

    page 137

--- ch03-hdfs/src/main/java/FileCopyWithProgress.java
+++ ch03-hdfs/src/main/java/FileCopyWithProgress.java
@@ -21,11 +21,7 @@ public class FileCopyWithProgress {
     
     Configuration conf = new Configuration();
     FileSystem fs = FileSystem.get(URI.create(dst), conf);
-    OutputStream out = fs.create(new Path(dst), new Progressable() {
-      public void progress() {
-        System.out.print(".");
-      }
-    });
+    OutputStream out = fs.create(new Path(dst), () -> System.out.println("."));
     
     IOUtils.copyBytes(in, out, 4096, true);
   }
